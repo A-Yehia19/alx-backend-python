05fd54582f1d28a03b1b28247310ca9884b9490e
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-06-11 22:09:02 +0300

    test case

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """module doc"""
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """function doc"""
     for i in range(10):
         await asyncio.sleep(1)
