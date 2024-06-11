5873522a9a01220231d2a50dcf41f5b080ca1bf4
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-06-11 17:54:01 +0300

    issues fixed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,5 +8,5 @@ async def wait_random(max_delay: int = 10) -> float:
     """function doc"""
 
     delay: float = random.uniform(0, max_delay)
-    asyncio.sleep(delay)
+    await asyncio.sleep(delay)
     return delay
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """function doc""" 
+    """function doc"""
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
     )
