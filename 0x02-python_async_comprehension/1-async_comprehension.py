68e9c1265547e8a020b7ece61d9c80723abf0e9c
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-06-12 16:41:39 +0300

    comprehension added

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,8 +8,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """function doc"""
 
-    result = []
-    async for i in async_generator():
-        result.append(i)
-
-    return result
+    return [num async for num in async_generator()]
